eaba782be5853ace7ff28d0e9f1390850154fa39
Albert Abril <alber@spottinstyle.com>
2015-04-27 23:49:40 +0200

    2nd version, alternative

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -5,22 +5,24 @@ var camera, scene, renderer;
 var geometry, material, mesh;
 
 var init = function () {
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer({alpha:true});
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.z = 800;
+    camera.position.z = 80;
     scene = new THREE.Scene();
-    geometry = new THREE.SphereGeometry( 200, 15, 10 );
-    material = new THREE.MeshBasicMaterial( { color: 0xDDA0DD, wireframe: true, wireframeLinewidth: 3 } );
+
+    geometry = new THREE.SphereGeometry( 840, 1, 60 );
+    material = new THREE.MeshBasicMaterial( { color: 0xB3DBFF, wireframe: true, wireframeLinewidth: 3 } );
+
     mesh = new THREE.Mesh( geometry, material );
     scene.add( mesh );
 }
 
 var animate = function () {
     requestAnimationFrame( animate );
-    mesh.rotation.x = Date.now() * 0.0002;
-    mesh.rotation.y = Date.now() * 0.001;
+    mesh.rotation.x = Date.now() * 0.00008;
+    mesh.rotation.y = Date.now() * 0.000010;
     renderer.render( scene, camera );
 }
 
